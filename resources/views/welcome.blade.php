2a659e282010801ebed3dee2ffe018fbe18cde63
nvaccina <niccolovac@gmail.com>
2023-06-05 14:28:06 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 9 + Bootstrap Template</title>
+
+        {{-- Includiamo gli assets con la direttiva @vite --}}
+
+    </head>
+    <body>
+
+    </body>
+</html>
