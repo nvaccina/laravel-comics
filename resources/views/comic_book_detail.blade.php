f83ef29d9f263f81e0c3f73d70dd9857b6bd35de
nvaccina <niccolovac@gmail.com>
2023-06-06 16:16:31 +0200

    Update layout comic_book_detail

--- resources/views/comic_book_detail.blade.php
+++ resources/views/comic_book_detail.blade.php
@@ -2,40 +2,81 @@
 
 @section('content')
 
-<main>
+<main class="nv_back">
     <div class="nv_blue"></div>
     <div class="book_detail">
 
-      <img class="locandina" src="{{$comic_book['thumb']}}" alt="{{$comic_book['title']}}">
+      <div class="locandina">
+        <img src="{{$comic_book['thumb']}}" alt="{{$comic_book['title']}}">
+        <span class="gallery">VIEW GALLERY</span>
+      </div>
 
-      <div class="book_text">
-        <div class="col">
-          <h4>{{$comic_book['title']}}</h4>
-          <div class="green_row">PRICE: {{$comic_book['price']}} | AVAIBLE | Cheeck Availability</div>
 
-          <p>{{$comic_book['description']}}</p>
-        </div>
+      <div class="book_text_top">
 
-        <div class="col">
-          <img src="" alt="">
-        </div>
+        <div class="col-8">
 
-        <div class="row">
-          <div class="col">
-            <table>
-              TABELLA 1
+          <h3>{{$comic_book['title']}}</h3>
 
-            </table>
+          <div class="green_row">
+            <div class="col-8">
+              <p>
+                <span class="nv_text_opacity">U.S. Price:</span> {{$comic_book['price']}}
+              </p>
+
+              <span class="nv_text_opacity">AVAILABLE</span>
+
+            </div>
+            <div class="col-4">
+              <span class="pr-10">Cheeck Availability</span>
+              <i class="fa-solid fa-caret-down"></i>
+            </div>
           </div>
-          <div class="col">
-            <table>
-              TABELLA 2
 
-            </table>
-          </div>
+          <p class="grey_color fs-14 ln-25">{{$comic_book['description']}}</p>
         </div>
 
+        <div class="col-4">
+          <h6>Advertisement</h6>
+          <img src="{{Vite::asset('resources/img/adv.jpg')}}" alt="">
+        </div>
+      </div>
+      <div class="info">
+        <div class="col-5">
+          <table>
+            <tr>
+              <th class="blue_color">Talent</th>
+            </tr>
+            <tr>
+              <td class="fs-12 fw-bold blue_color">Art by:</td>
+              <td class="fs-12 fw-bold acqua_color pl-40">{{implode(' - ', $comic_book['artists'])}}</td>
+            </tr>
+            <tr>
+              <td class="fs-12 fw-bold blue_color">Written by:</td>
+              <td class="fs-12 fw-bold acqua_color pl-40">{{implode(' - ', $comic_book['writers'])}}</td>
+            </tr>
+          </table>
+        </div>
+        <div class="col-5">
+          <table class="pl-30">
+            <tr>
+              <th class="blue_color">Specs</th>
+            </tr>
+            <tr>
+              <td class="fs-12 fw-bold blue_color">Series:</td>
+              <td class="fs-12 fw-bold acqua_color pl-40">{{$comic_book['series']}}</td>
+            </tr>
+            <tr>
+              <td class="fs-12 fw-bold blue_color">U.S. Price:</td>
+              <td class="fs-12 pl-40">{{$comic_book['price']}}</td>
+            </tr>
+            <tr>
+              <td class="fs-12 fw-bold blue_color">On Sale Date:</td>
+              <td class="fs-12 pl-40">{{$comic_book['sale_date']}}</td>
+            </tr>
+          </table>
 
+        </div>
       </div>
 
     </div>
